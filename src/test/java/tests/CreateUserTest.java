a14f2cca496480567fe02ddd28522152b35b0327
Ana Katamadze <ana.katamadze.5@iliauni.edu.ge>
2026-03-15 23:55:39 +0400

    Added my name

--- src/test/java/tests/CreateUserTest.java
+++ src/test/java/tests/CreateUserTest.java
@@ -24,7 +24,7 @@ public class CreateUserTest {
     public void createUser() {
         User user = new User();
         user.setUsername(RandomUtils.getRandomAlphabeticString());
-        user.setFirstName("Ana");
+        user.setFirstName("Anaa");
         user.setLastName("Katamadze");
         user.setEmail(RandomUtils.getRandomAlphabeticString());
         user.setPassword(RandomUtils.getRandomAlphabeticString());
